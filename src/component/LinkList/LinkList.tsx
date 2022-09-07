a6d6649a6b2b5b3fb8040ff88527d9ea6dc2f0b1
riki3248 <rw740643@gmail.com>
2022-09-07 11:31:53 +0300

    try

---
+++ src/component/LinkList/LinkList.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import "./LinkList.css";
+
+class LinkList extends Component<{props:any}>  {
+    // console.log( props);
+    
+ 
+    
+     linkMarkup = this.props.options.map((link:any) => (
+        <li key={link.id} className="link-list-item">
+            <a
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="link-list-item-url"
+            >
+                {link.text}
+            </a>
+        </li>
+    ));
+    render() {
+    
+        return (
+         <ul className="link-list">{this.linkMarkup}</ul>)
+    }
+    
+};
+export default LinkList;
